54ca99735533f1cc786a59b61a0a0ab43d606103
xiaotianxt <me@xiaotian.dev>
2026-04-29 18:02:09 -0400

    cx 0.1.1

--- Formula/cx.rb
+++ Formula/cx.rb
@@ -1,15 +1,24 @@
 class Cx < Formula
   desc "Fast local Codex launcher, stdin wrapper, and slot manager"
   homepage "https://github.com/xiaotianxt/cx"
-  url "https://github.com/xiaotianxt/cx/releases/download/v0.1.0/cx-v0.1.0-darwin-arm64.tar.gz"
-  version "0.1.0"
-  sha256 "85892a4f137f23328ad73365a7511d99661850e722111f78963d68046d01d7ed"
+  url "https://github.com/xiaotianxt/cx/releases/download/v0.1.1/cx-v0.1.1-darwin-arm64.tar.gz"
+  sha256 "85d16313e664831a65174877c363b104cea1f2c38c7e58c468f39300cd903079"
   license "MIT"
+  version "0.1.1"
 
   depends_on arch: :arm64
 
+  head do
+    url "https://github.com/xiaotianxt/cx.git", branch: "main"
+    depends_on "rust" => :build
+  end
+
   def install
-    bin.install "cx"
+    if build.head?
+      system "cargo", "install", "--bin", "cx", "--root", prefix, "."
+    else
+      bin.install "cx"
+    end
   end
 
   test do
