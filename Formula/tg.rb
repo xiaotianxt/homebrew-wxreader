a94ebf0cf285461562e484e142a1e514a541e7fd
Andy Tian <me@xiaotian.dev>
2026-04-29 15:23:43 -0400

    tg 1.1.2

--- Formula/tg.rb
+++ Formula/tg.rb
@@ -1,22 +1,29 @@
 class Tg < Formula
   desc "macOS Telegram 聊天记录读取 CLI 工具"
   homepage "https://github.com/xiaotianxt/tg"
-  url "https://github.com/xiaotianxt/tg.git",
-      tag:      "v1.1.1",
-      revision: "035917988ab4b8b419ce53c55fc64930bb497849"
+  url "https://github.com/xiaotianxt/tg/releases/download/v1.1.2/tg-v1.1.2-darwin-arm64.tar.gz"
+  sha256 "190526619f9e1a42d6107ac4c79a3c41255b6018f3bc99e365ca79adc2401df4"
   license "MIT"
+  version "1.1.2"
 
-  head "https://github.com/xiaotianxt/tg.git", branch: "main"
-
-  depends_on "rust" => :build
   depends_on arch: :arm64
 
+  head do
+    url "https://github.com/xiaotianxt/tg.git", branch: "main"
+    depends_on "rust" => :build
+  end
+
   def install
-    system ENV.cc, "-O2", "-o", "scanner_macos",
-           "vendor/find_all_keys_macos.c",
-           "-framework", "Foundation"
-    system "cargo", "install", "--bin", "tg", "--root", prefix, "."
-    bin.install "scanner_macos"
+    if build.head?
+      system ENV.cc, "-O2", "-o", "scanner_macos",
+             "vendor/find_all_keys_macos.c",
+             "-framework", "Foundation"
+      system "cargo", "install", "--bin", "tg", "--root", prefix, "."
+      bin.install "scanner_macos"
+    else
+      bin.install "tg"
+      bin.install "scanner_macos"
+    end
   end
 
   test do
