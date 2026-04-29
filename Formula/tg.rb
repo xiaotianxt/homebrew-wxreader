6236885e488ab0ed66f4e4407708e483be0125aa
xiaotianxt <me@xiaotian.dev>
2026-04-29 15:45:01 -0400

    tg 1.1.3

--- Formula/tg.rb
+++ Formula/tg.rb
@@ -1,16 +1,15 @@
 class Tg < Formula
   desc "macOS Telegram 聊天记录读取 CLI 工具"
   homepage "https://github.com/xiaotianxt/tg"
-  url "https://github.com/xiaotianxt/tg/releases/download/v1.1.2/tg-v1.1.2-darwin-arm64.tar.gz"
-  version "1.1.2"
-  sha256 "190526619f9e1a42d6107ac4c79a3c41255b6018f3bc99e365ca79adc2401df4"
+  url "https://github.com/xiaotianxt/tg/releases/download/v1.1.3/tg-v1.1.3-darwin-arm64.tar.gz"
+  version "1.1.3"
+  sha256 "fa2901ea2b22d34ebe92bb2f44d4aa1cacf80153c0f7cd621d62183ff4585319"
   license "MIT"
 
   depends_on arch: :arm64
 
   def install
     bin.install "tg"
-    bin.install "scanner_macos"
   end
 
   test do
