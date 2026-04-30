604e03de0566405714d531d13a2919363fc4908a
xiaotianxt <me@xiaotian.dev>
2026-04-29 22:09:32 -0400

    tg 1.1.6

--- Formula/tg.rb
+++ Formula/tg.rb
@@ -1,9 +1,9 @@
 class Tg < Formula
   desc "macOS Telegram 聊天记录读取 CLI 工具"
   homepage "https://github.com/xiaotianxt/tg"
-  url "https://github.com/xiaotianxt/tg/releases/download/v1.1.5/tg-v1.1.5-darwin-arm64.tar.gz"
-  version "1.1.5"
-  sha256 "a2d7787dac321587134607c139824200774ff4141e11d3809510bcde1e40e57b"
+  url "https://github.com/xiaotianxt/tg/releases/download/v1.1.6/tg-v1.1.6-darwin-arm64.tar.gz"
+  version "1.1.6"
+  sha256 "6fc7137ed01e461382e04afce0118959a78aca62f92763a3b1b4f10c47c5b72c"
   license "MIT"
 
   depends_on arch: :arm64
