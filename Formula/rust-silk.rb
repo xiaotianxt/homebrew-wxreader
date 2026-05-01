3353e2c0d63b512c8476238ce6b14192bd0ae949
xiaotianxt <me@xiaotian.dev>
2026-04-30 22:05:33 -0400

    rust-silk 0.1.0 bottle

--- Formula/rust-silk.rb
+++ Formula/rust-silk.rb
@@ -5,6 +5,12 @@ class RustSilk < Formula
   sha256 "ff872e7d5808b092b18d8a696f4cd45717d03fefeac498d535aca84461184559"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/xiaotianxt/homebrew-tap/releases/download/rust-silk-0.1.0"
+    rebuild 1
+    sha256 cellar: :any, arm64_sequoia: "b252da47ef28407175aa95ea991441b252a43977a2bc66a7bee91403a04fb012"
+  end
+
   depends_on "rust" => :build
 
   def install
